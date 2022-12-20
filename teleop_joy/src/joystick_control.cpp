e220f389f6fdff5c13174caf3f4e7fd2429a7821
Masatake Aoki <aoki@rt-net.jp>
2022-12-20 20:54:50 +0900

    change name pico_direction_controller to teleop_joy

--- teleop_joy/src/joystick_control.cpp
+++ teleop_joy/src/joystick_control.cpp
@@ -1,3 +1,17 @@
+// Copyright 2022 RT Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
@@ -12,14 +26,14 @@ void callback(const sensor_msgs::msg::Joy::SharedPtr data)
   if(data->axes[7] != 0 ){
     linear_x = 150*data->axes[7];
   }else if((data->axes[1] > 0.00001) || (data->axes[1] < -0.00001)){
-    linear_x = data->axes[1]*1000.0;
+    linear_x = data->axes[1]*500.0;
   }else{
     linear_x = 0;
   } 
   if(data->axes[6] != 0){
-    angular_z = 250*data->axes[6];
+    angular_z = 50*data->axes[6];
   }else if((data->axes[3] > 0.00001) || (data->axes[3] < -0.00001)){ 
-    angular_z = data->axes[3]*500.0;
+    angular_z = data->axes[3]*100.0;
   }else{
     angular_z = 0;
   }
