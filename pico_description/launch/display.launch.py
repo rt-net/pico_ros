1ef69fde50b1d51ce3587ff05077e34bddef8026
Shota Aoki <s.aoki@rt-net.jp>
2023-06-15 14:31:06 +0900

    run_jsp_guiオプションをlaunchファイルに追加 (#3)

--- pico_description/launch/display.launch.py
+++ pico_description/launch/display.launch.py
@@ -16,11 +16,19 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch.substitutions import Command
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    declare_arg_run_jsp_gui = DeclareLaunchArgument(
+        'run_jsp_gui',
+        default_value='false',
+        description='Set true to run joint_state_publisher_gui node.')
+
     xacro_file = os.path.join(
         get_package_share_directory('pico_description'),
         'urdf',
@@ -41,6 +49,7 @@ def generate_launch_description():
       package="joint_state_publisher_gui",
       executable="joint_state_publisher_gui",
       output="screen",
+      condition=IfCondition(LaunchConfiguration('run_jsp_gui'))
     )
 
     rviz_node = Node(
@@ -52,6 +61,7 @@ def generate_launch_description():
     )
 
     nodes = [
+      declare_arg_run_jsp_gui,
       rviz_node,
       robot_state_pub_node,
       joint_state_pub_gui_node
